283a0639c034556c6d2c97e90ac857d37c16f611
lsqproduction <lilysway@hotmail.com>
2018-12-31 04:57:11 +0530

    text updates

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Liang Liang", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Minimalist.", "Simplified.", "Optimist.", "Perfectionist.", "Humble."],
       typeSpeed: 100,
       loop: true,
     });
